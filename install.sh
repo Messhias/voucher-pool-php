9e5047eab95ca9178325f4db06bd031b5d7cc4ed
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-07-28 13:33:17 -0300

    Updated the enviroment installation

--- install.sh
+++ install.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-
 clear
 
 echo "Installing your application."
 
 echo "Hi, $USER"
 
+cd backend
 
 echo "Installing containers."
 docker build .
 
 echo "Installing backend and running tests."
-cd backend && composer install && cp .env.example .env && php artisan migrate && ./vendor/bin/phpunit
+cd composer install && cp .env.example .env && php artisan migrate && ./vendor/bin/phpunit
 cd ..
 
-echo "Installing frontend."
-cd frontend && npm install
-cd ..
-
-clear
-
-echo "Installed successful!!"
+# echo "Installing frontend."
+# cd frontend && npm install
+# cd ..
+#
+# clear
+#
+# echo "Installed successful!!"
 echo "Running your application..."
+
+cd backend
 
 docker-compose up -d
 
-cd frontend && npm start
+# cd frontend && npm start
